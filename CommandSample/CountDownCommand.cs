ee5b6abd8957d5370bfb30ac780db4b601c27a34
hagi845 <89743047+hagi845@users.noreply.github.com>
2022-06-20 01:29:14 +0900

    カウンターの実装

---
+++ CommandSample/CountDownCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CommandSample
+{
+    public class CountDownCommand : ICommand
+    {
+        private CounterViewModel _viewModel { get; set; }
+
+        public CountDownCommand(CounterViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.Value = (_viewModel.Value <= 0) ? 100 : _viewModel.Value - 1;
+        }
+    }
+
+}
---
+++ CommandSample/CounterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CommandSample
+{
+    public class Counter
+    {
+        public int Value { get; set; } = 100;
+    }
+
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Counter Counter { get; set; }
+
+        public CountDownCommand CountDownCommand { get; private set; }
+
+        public CounterViewModel()
+        {
+            CountDownCommand = new CountDownCommand(this);
+            Counter = new Counter();
+        }
+
+        public int Value
+        {
+            get
+            {
+                return Counter.Value;
+            }
+            set
+            {
+                Counter.Value = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Value"));
+                }
+            }
+        }
+    }
+}
